5c33a88e2ae0f42e29735488ffe2d6588494d73f
Ronak-Programs <ronakrathva00@gmail.com>
2025-08-28 23:20:40 +0530

    Add files via upload

---
+++ 12th Convert kgs into gram.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float kg;
+
+    printf("Enter the value of kg : ");
+    scanf("%f",&kg);
+
+    float gm = kg*1000;
+    printf("value of gram is : %f",gm);
+
+    return 0;
+
+}
