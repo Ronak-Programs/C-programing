870d8d5a133df87bc01fce7be2aedfe3f30967eb
Ronak-Programs <ronakrathva00@gmail.com>
2025-08-28 23:32:05 +0530

    Add files via upload

---
+++ 15th Convert fahrenheit to celsius.c
@@ -0,0 +1,12 @@
+  #include<stdio.h>
+  int main()
+  {
+      float feh;
+      printf("Enter the value of fahrenheit : ");
+      scanf("%f",&feh);
+
+      float cel = (0.56)*(feh-32);
+      printf("value of celcius is : %f", cel);
+
+      return 0;
+  }
