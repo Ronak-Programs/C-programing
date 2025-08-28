140081f6ac597f9af9e8ff4062cc6d16e6afd364
Ronak-Programs <ronakrathva00@gmail.com>
2025-08-28 23:41:33 +0530

    Add files via upload

---
+++ 20th Find area of triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float l;
+    printf("Enter the value of l : ");
+    scanf("%f", &l);
+
+    float h;
+    printf("Enter the value of h : ");
+    scanf("%f", &h);
+
+    float area;
+    printf("Area of triangle is : %f", 0.5*l*h);
+
+    return 0;
+}
