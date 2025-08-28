cbf0834ab3a064f7d30c3644ea390b36a1f96b25
Ronak-Programs <ronakrathva00@gmail.com>
2025-08-28 23:35:53 +0530

    Add files via upload

---
+++ 17th Find area & perimeter of square.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+ {
+     int l;
+     printf("Enter the value of l : ");
+     scanf("%d",&l);
+
+     int area;
+     printf("Area of square is : %d\n", l*l);
+
+     int p_mtr;
+     printf("Perimeter of Square is : %d\n", 4*l);
+
+     return 0;
+ }
