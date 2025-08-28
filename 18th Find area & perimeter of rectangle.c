19d0f152ea35258371ccb8cc39e2c1bf993ecb86
Ronak-Programs <ronakrathva00@gmail.com>
2025-08-28 23:37:22 +0530

    Add files via upload

---
+++ 18th Find area & perimeter of rectangle.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ int main()
+ {
+     int l;
+     printf("Enter the value of l :");
+     scanf("%d", &l);
+
+     int b;
+     printf("Enter the value of b :");
+     scanf("%d", &b);
+
+     int area;
+     printf("Area of rectangle is : %d\n", l*b);
+
+     int p_mtr;
+     printf("Perimeter of rectangle is : %d\n", 2*(l+b));
+
+     return 0;
+
+ }
